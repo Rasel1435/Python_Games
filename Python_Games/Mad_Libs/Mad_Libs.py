8a058948259c416723b2863929fe577d09b7ceb0
Sheikh Rasel Ahmed <91475469+Rasel1435@users.noreply.github.com>
2022-03-03 03:39:27 -0600

    Games All

---
+++ Python_Games/Mad_Libs/Mad_Libs.py
@@ -0,0 +1,7 @@
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+celebrity = input("Enter a celebrity: ")
+
+print("Roses are " + color)
+print(plural_noun + "are blue")
+print("I Love " + celebrity)
